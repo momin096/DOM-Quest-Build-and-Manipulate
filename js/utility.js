02965d0092e8ed3bb2660e24b124892840417790
momin096 <momin09609@gamil.com>
2024-12-08 18:37:10 +0600

    donate section full added

--- js/utility.js
+++ js/utility.js
@@ -3,4 +3,16 @@ function getInputValueById(elementId){
     const inputValue = document.getElementById(elementId).value ;
     const input = parseFloat(inputValue);
     return input;
+};
+
+function getTextValueById(elementId){
+    const textValue = document.getElementById(elementId).innerText;
+    const text = parseFloat(textValue);
+    return text ;
+
+}
+
+function setInnerTextById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
 }
